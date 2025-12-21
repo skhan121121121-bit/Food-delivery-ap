b87ac730193c07a6be8385e020542bd208c52fda
skhan121121121-bit <skhan121121121@gmail.com>
2025-12-21 17:02:07 +0530

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,7 @@
-const CACHE_NAME = "food-app-v1";
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener("install", () => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("activate", () => {
+  self.clients.claim();
 });
